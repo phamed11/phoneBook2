e01b891336b076b6bffad30626a0f5355cac7d84
Peter <wrkpeter@gmail.com>
2019-02-07 11:09:27 +0100

    integrations tests

--- src/test/java/com/phoneBook2/services/ContactServiceIntegrationTests.java
+++ src/test/java/com/phoneBook2/services/ContactServiceIntegrationTests.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
